3d74187eba95e7b8a6681aa00f68dc85ebd985f4
Salman Khaskheli <salmankhaskheli852@gmail.com>
2025-12-16 23:52:36 +0000

    walet ke andar kuch bhi na ho

--- src/app/wallet/page.tsx
+++ src/app/wallet/page.tsx
@@ -1,6 +1,5 @@
 import { WalletClient } from "./WalletClient";
-import { transactions, adminWallets } from "@/lib/data";
 
 export default function WalletPage() {
-  return <WalletClient transactions={transactions} adminWallets={adminWallets} />;
+  return <WalletClient />;
 }
