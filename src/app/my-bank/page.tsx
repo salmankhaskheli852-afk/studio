21003c975f555b5594bd54b9d00c1b6275a99e3c
Salman Khaskheli <salmankhaskheli852@gmail.com>
2025-12-16 23:32:38 +0000

    React.Children.only expected to receive a single React element child.
    s

--- src/app/my-bank/page.tsx
+++ src/app/my-bank/page.tsx
@@ -283,21 +283,21 @@ export default function MyBankPage() {
                         render={({ field }) => (
                         <FormItem>
                             <FormLabel>Choose withdrawal method</FormLabel>
-                            <Select onValueChange={(value: 'wallet' | 'bank') => {
-                                field.onChange(value);
-                                setWithdrawalMethod(value);
-                                withdrawForm.setValue('bankName', '');
-                            }} defaultValue={field.value}>
                             <FormControl>
-                                <SelectTrigger>
-                                    <SelectValue placeholder="Select a method" />
-                                </SelectTrigger>
+                                <Select onValueChange={(value: 'wallet' | 'bank') => {
+                                    field.onChange(value);
+                                    setWithdrawalMethod(value);
+                                    withdrawForm.setValue('bankName', '');
+                                }} defaultValue={field.value}>
+                                    <SelectTrigger>
+                                        <SelectValue placeholder="Select a method" />
+                                    </SelectTrigger>
+                                    <SelectContent>
+                                        <SelectItem value="wallet">Wallet account (Jazzcash/Easypaisa)</SelectItem>
+                                        <SelectItem value="bank">Bank account</SelectItem>
+                                    </SelectContent>
+                                </Select>
                             </FormControl>
-                            <SelectContent>
-                                <SelectItem value="wallet">Wallet account (Jazzcash/Easypaisa)</SelectItem>
-                                <SelectItem value="bank">Bank account</SelectItem>
-                            </SelectContent>
-                            </Select>
                             <FormMessage />
                         </FormItem>
                         )}
@@ -314,13 +314,13 @@ export default function MyBankPage() {
                                     <SelectTrigger>
                                         <SelectValue placeholder={`Select a ${withdrawalMethod === 'wallet' ? 'wallet' : 'bank'}`} />
                                     </SelectTrigger>
-                                <SelectContent>
-                                {currentOptions.map((option) => (
-                                    <SelectItem key={option} value={option}>
-                                    {option}
-                                    </SelectItem>
-                                ))}
-                                </SelectContent>
+                                    <SelectContent>
+                                    {currentOptions.map((option) => (
+                                        <SelectItem key={option} value={option}>
+                                        {option}
+                                        </SelectItem>
+                                    ))}
+                                    </SelectContent>
                                 </Select>
                             </FormControl>
                             <FormMessage />
