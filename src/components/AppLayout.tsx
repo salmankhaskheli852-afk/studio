a258fd1d479610772174d1d11d7c87c7b7402667
Salman Khaskheli <salmankhaskheli852@gmail.com>
2025-12-17 00:21:48 +0000

    iska beckground mtlb wallpaper dosra collor kro ye nhi  (_for element ma

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -197,7 +197,7 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
             )}
           </div>
         </header>
-        <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6 bg-muted/40">
+        <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6 bg-background">
           {children}
         </main>
       </div>
